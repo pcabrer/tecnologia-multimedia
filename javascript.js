e20e029c070d3e2b0d429ee2ec92ec9d67a4b691
Marc Melià <marcmelia10@gmail.com>
2023-05-09 12:44:55 +0200

    lista monumentos actualizada

--- javascript.js
+++ javascript.js
@@ -16,8 +16,7 @@ function cargaListaMonumentos() {
 
             datos.itemListElement.forEach(function (monumento) {
 
-                console.log(monumento.name);
-                console.log(monumento.image[0].url);
+
 
                 /*
                 <div class="col-lg-4 col-md-6 portfolio-item filter-palma">
@@ -36,57 +35,111 @@ function cargaListaMonumentos() {
                     </div>
                 </div>
               */
-                // Creamos los elementos HTML
-                const colDiv = document.createElement('div');
-                colDiv.classList.add('col-lg-4', 'col-md-6', 'portfolio-item', 'filter-palma');
+                // Crear elemento div con clases col-12 col-md-12 col-lg-6 mr-lg-3 justify-content-center
+                const div = document.createElement('div');
+                div.classList.add('col-12', 'col-md-12', 'col-lg-6', 'mr-lg-3', 'justify-content-center');
 
-                const wrapDiv = document.createElement('div');
-                wrapDiv.classList.add('portfolio-wrap');
+                // Crear enlace con atributos href y clase monumentos-card-link
+                const enlace = document.createElement('a');
+                enlace.setAttribute('href', 'catedral.html');
+                enlace.classList.add('monumentos-card-link');
 
-                const img = document.createElement('img');
-                img.src = monumento.image[0].url;
-                img.classList.add('img-fluid');
-                img.alt = '';
+                // Crear elemento div con clases card mi-carta
+                const divCard = document.createElement('div');
+                divCard.classList.add('card', 'mi-carta');
 
-                const infoDiv = document.createElement('div');
-                infoDiv.classList.add('portfolio-info');
+                // Crear imagen con atributo src y clase monumentos-card-imagen
+                const imagen = document.createElement('img');
+                imagen.setAttribute('src', monumento.image[0].url);
+                imagen.classList.add('monumentos-card-imagen');
 
-                const linkA = document.createElement('a');
-                linkA.href = 'catedral.html';
+                // Crear elemento div con clase monumentos-card-contenido
+                const divContenido = document.createElement('div');
+                divContenido.classList.add('monumentos-card-contenido');
 
-                const titleH4 = document.createElement('h4');
-                titleH4.textContent = monumento.name;
+                // Crear título h4 con clase monumentos-card-titulo y texto Catedral de Palma
+                const titulo1 = document.createElement('h4');
+                titulo1.classList.add('monumentos-card-titulo');
+                titulo1.textContent = monumento.name;
 
-                const locationP = document.createElement('p');
-                locationP.textContent = monumento.address.addressLocality;
+                // Crear título h5 con clase monumentos-card-titulo y texto Palma
+                const titulo2 = document.createElement('h5');
+                titulo2.classList.add('monumentos-card-ciudad');
+                titulo2.textContent = monumento.address.addressLocality;
 
-                const linksDiv = document.createElement('div');
-                linksDiv.classList.add('portfolio-links');
+                // Crear párrafo con clase monumentos-card-texto y texto Lorem ipsum
+                const parrafo = document.createElement('p');
+                parrafo.classList.add('monumentos-card-descripcion');
 
-                const lightboxA = document.createElement('a');
-                lightboxA.href = monumento.image[0].url;
-                lightboxA.dataset.gallery = 'portfolioGallery';
-                lightboxA.classList.add('portfolio-lightbox');
+                const palabras = monumento.description.split(' ').slice(0, 40); // Obtener las primeras 10 palabras
+                const texto = palabras.join(' '); // Unir las palabras seleccionadas
+                parrafo.textContent = texto + '...';
 
-                const plusIcon = document.createElement('i');
-                plusIcon.classList.add('bx', 'bx-plus');
+                // Crear el elemento para mostrar si está abierto o cerrado
+                const openStatusElement = document.createElement("p");
 
-                // Agregamos los elementos al árbol del DOM
-                linksDiv.appendChild(lightboxA);
-                lightboxA.appendChild(plusIcon);
+                if (estaAbierto(monumento.openingHours)) {
+                    openStatusElement.classList.add("monumentos-card-abierto");
+                    openStatusElement.textContent = "Abierto";
 
-                linkA.appendChild(titleH4);
 
-                infoDiv.appendChild(linkA);
-                infoDiv.appendChild(locationP);
-                infoDiv.appendChild(linksDiv);
+                } else {
 
-                wrapDiv.appendChild(img);
-                wrapDiv.appendChild(infoDiv);
+                    openStatusElement.classList.add("monumentos-card-cerrado");
+                    openStatusElement.textContent = "Cerrado";
 
-                colDiv.appendChild(wrapDiv);
+                }
 
-                monumentList.appendChild(colDiv);
+
+
+                // creamos el elemento div con clase "stars"
+                const starsDiv = document.createElement("div");
+                starsDiv.classList.add("stars");
+
+                // creamos el elemento span con clase "rating" y contenido de texto "4,1"
+                var ratingSpan = document.createElement("span");
+                ratingSpan.classList.add("rating");
+                ratingSpan.innerText = monumento.aggregateRating.ratingValue;
+
+                var numero = parseFloat(monumento.aggregateRating.ratingValue);
+                var entero = Math.floor(numero); // obtiene la parte entera (4)
+                var decimal = parseInt((numero - entero) * 10); // obtiene la parte decimal (7)
+                console.log(numero);
+                starsDiv.appendChild(ratingSpan);
+
+                // creamos cinco elementos span con clase "star", de los cuales los primeros cuatro tienen clase "checked" y contenido de texto "&#9733;"
+                for (var i = 0; i < 5; i++) {
+                    var starSpan = document.createElement("span");
+                    starSpan.classList.add("star");
+                    if (i < entero) {
+                        starSpan.classList.add("checked");
+                        
+                    } else if(decimal>=5) {
+                        starSpan.classList.add("halfchecked");
+                        decimal=0;
+
+                    }
+                    starSpan.innerHTML = "&#9733;";
+                    starsDiv.appendChild(starSpan);
+
+
+                }
+
+
+
+
+                // Agregar todos los elementos creados a sus elementos contenedores correspondientes
+                divContenido.appendChild(titulo1);
+                divContenido.appendChild(titulo2);
+                divContenido.appendChild(parrafo);
+                divContenido.appendChild(starsDiv);
+                divContenido.appendChild(openStatusElement);
+                divCard.appendChild(imagen);
+                divCard.appendChild(divContenido);
+                enlace.appendChild(divCard);
+                div.appendChild(enlace);
+
+                monumentList.appendChild(div);
 
 
             });
@@ -96,6 +149,101 @@ function cargaListaMonumentos() {
         }
 
     };
-   
+
     xhr.send();
+}
+
+function estaAbierto(horario) {
+
+
+    const dateActual = new Date();
+    const diaActual = dateActual.getDay();
+
+    const diasSemana = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+
+    for (let i = 0; i < horario.length; i++) {
+
+        const cadenadias = horario[i].split(" "); // separar la cadena en dos partes utilizando el espacio como separador
+
+        const dias = cadenadias[0]; // primera parte de la cadena
+        const horas = cadenadias[1]; // segunda parte de la cadena
+        const hora = horas.split("-");
+
+        // Separamos las horas y los minutos
+        const horaApertura = hora[0].split(":");
+        const horaCierre = hora[1].split(":");
+
+        const horaAperturaI = parseInt(horaApertura[0]);
+        const minutosAperturaI = parseInt(horaApertura[1]);
+
+        const horaCierreI = parseInt(horaCierre[0]);
+        const minutosCierreI = parseInt(horaCierre[1]);
+
+        // Creamos un objeto Date con la hora
+        const horaAperturaDate = new Date();
+        horaAperturaDate.setHours(horaAperturaI);
+        horaAperturaDate.setMinutes(minutosAperturaI);
+        horaAperturaDate.setSeconds(0);
+
+        // Creamos un objeto Date con la hora
+        const horaCierreDate = new Date();
+        horaCierreDate.setHours(horaCierreI);
+        horaCierreDate.setMinutes(minutosCierreI);
+        horaCierreDate.setSeconds(0);
+
+
+        if (dias.includes('-')) {
+
+            const diaJ = dias.split("-");
+
+            const indiceDia1 = diasSemana.indexOf(diaJ[0]);
+            const indiceDia2 = diasSemana.indexOf(diaJ[1]);
+
+            if (indiceDia1 != -1 && indiceDia2 != -1) {
+
+                var indice = indiceDia1;
+                var indicefinal = (indiceDia2 + 1) % 7;
+
+                do {
+
+                    if (indice == diaActual && horaAperturaDate < dateActual && horaCierreDate > dateActual) {
+
+                        return true;
+
+                    }
+
+                    indice = (indice + 1) % 7;
+
+                } while (indice != indicefinal);
+
+            }
+        } else if (dias.includes(',')) {
+
+
+            const diasSemanaT = dias.split(",");
+
+            for (let j = 0; j < diasSemana.length; j++) {
+                console.log(diasSemanaT[j]);
+                const indiceDia12 = diasSemana.indexOf(diasSemanaT[j]);
+
+                if (indiceDia12 == diaActual && horaAperturaDate < dateActual && horaCierreDate > dateActual) {
+
+                    return true;
+                }
+
+            }
+
+
+        } else {
+
+            const indiceDia = diasSemana.indexOf(dias);
+            if (indiceDia == diaActual && horaAperturaDate < dateActual && horaCierreDate > dateActual) {
+
+                return true;
+            }
+
+        }
+    }
+
+    return false;
 }
