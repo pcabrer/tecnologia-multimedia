e62ecb4cdd1aee48de88574c7d4b5595b5ae5880
pcabrer <p.cabrer.007@hotmail.es>
2023-05-09 23:52:55 +0200

    Añadido el mapa con las chinchetas que indican donde está cada monumento

--- mapa.js
+++ mapa.js
@@ -1,41 +1,68 @@
 
 
-function loadedPage() {
+function mapaLeaflet() {
+    // div que contiene la lista de monumentos
 
-    var monumentos = [
-        {
-            "@context": "http://schema.org",
-            "@type": "TouristAttraction",
-            "name": "La Sagrada Familia",
-            "geo": {
-                "@type": "GeoCoordinates",
-                "latitude": 41.4036,
-                "longitude": 2.1744
-            }
-        },
-        {
-            "@context": "http://schema.org",
-            "@type": "TouristAttraction",
-            "name": "Park Güell",
-            "geo": {
-                "@type": "GeoCoordinates",
-                "latitude": 41.4145,
-                "longitude": 2.1527
-            }
-        }
-    ];
-
-
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
+    var mymap = L.map('map').setView([39.6, 3.0176], 9.5);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
         maxZoom: 18,
     }).addTo(mymap);
 
-    monumentos.forEach(function (monumento) {
-        var marker = L.marker([monumento.geo.latitude, monumento.geo.longitude]).addTo(mymap);
-    });
+    const monumentList = document.getElementById('contenedor-monumentos');
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "Monumentos.json", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            //console.log(xhr.responseText);
+            var datos = JSON.parse(xhr.responseText);
 
-    marker.bindPopup(monumento.name);
+            /*
+            var markersLayer = L.layerGroup();
+
+            // Iterar sobre los monumentos y agregar un marcador para cada uno
+            datos.itemListElement.forEach(function (monumento) {
+                var marker = L.marker([monumento.geo.latitude, monumento.geo.longitude]);
+
+                // Crear el contenido del popup con el nombre del monumento y el enlace al HTML correspondiente
+                var popupContent = '<div>' +
+                    '<h3>' + monumento.name + '</h3>' +
+                    '<a href="monumento.html?id=' + monumento.id + '">Ver detalles</a>' +
+                    '</div>';
+
+                // Establecer el contenido del popup y un controlador de eventos para abrir el HTML correspondiente al hacer clic en el popup
+                marker.bindPopup(popupContent).on('click', function () {
+                    window.location.href = 'detalle.html?id=' + monumento.id;
+                });
+
+                // Agregar el marcador a la capa de marcadores
+                markersLayer.addLayer(marker);
+            });
+
+            // Agregar la capa de marcadores al mapa
+            markersLayer.addTo(map);
+            */
+            
+            datos.itemListElement.forEach(function (monumento, layer) {
+
+                
+                var marker = L.marker([monumento.geo.latitude, monumento.geo.longitude]).addTo(mymap);
+                marker.bindPopup(monumento.name);
+              
+            });
+            
+
+
+
+        }
+
+    };
+
+    xhr.send();
 }
+
+
+
+
+
+
