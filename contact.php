f8281e8ffd64846444cfb7cd32c31aa26368ef57
pcabrer <p.cabrer.007@hotmail.es>
2023-05-31 21:02:20 +0200

    Pequeño Cambio en el php

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
     $mensaje = $_POST['mensaje'];
 
     // Configura los detalles del correo electrónico
-    $destinatario = 'soporte@monumentosmallorca.com';
+    $destinatario = 'p.cabrer.007@hotmail.es';
     $asunto = 'Nuevo mensaje de contacto';
 
     // Construye el cuerpo del mensaje
@@ -16,17 +16,20 @@
     $cuerpoMensaje .= "Asunto: $sujeto\n";
     $cuerpoMensaje .= "Mensaje: $mensaje\n";
 
+    /*
     $to = "p.cabrer.007@hotmail.es";
     $subject = "Checking PHP mail";
     $message = "PHP mail works just fine";
     mail($to,$subject,$message);
+    */
+
     //echo "The email message was sent.";
 
 
-    /*
+    
     // Envía el correo electrónico
     mail($destinatario, $asunto, $cuerpoMensaje);
-    */
+    
 
     // Muestra un mensaje de éxito en tu página HTML
     echo '<script>alert("Tu mensaje ha sido enviado. ¡Gracias!");</script>';
