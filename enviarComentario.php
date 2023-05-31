f8dda512318c2d3917081c82a6e728a2f35b6d94
Marc Melià <marcmelia10@gmail.com>
2023-05-31 19:40:00 +0200

    act77

--- enviarComentario.php
+++ enviarComentario.php
@@ -37,6 +37,39 @@ $data['itemListElement'][] = $newItem;
 $newJson = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 file_put_contents('../public/valoraciones.json', $newJson);
 
+
+
+
+// Cargar el JSON existente
+$jsonMonumento = file_get_contents('../public/Monumentos.json');
+$dataMonumento = json_decode($jsonMonumento, true);
+
+// Recorrer las reseñas y buscar el monumento con nombre "Catedral de Palma"
+foreach ($dataMonumento['itemListElement'] as &$item) {
+    if ($item['name'] == $name) {
+        // Actualizar los valores de "ratingValue" y "reviewCount"
+
+        $mediaValoraciones  = floatval($item['aggregateRating']['ratingValue']);
+        $numeroValoraciones  = floatval($item['aggregateRating']['reviewCount']);
+
+        $nuevaMedia = ($mediaValoraciones*$numeroValoraciones + floatval($rating))/($numeroValoraciones+1);
+
+        $numeroValoraciones++;
+
+        $item['aggregateRating']['ratingValue'] = $nuevaMedia;
+        $item['aggregateRating']['reviewCount'] =  $numeroValoraciones;
+    }
+}
+
+
+// Codificar el array actualizado de nuevo a JSON
+$jsonUpdated = json_encode($dataMonumento, JSON_PRETTY_PRINT| JSON_UNESCAPED_UNICODE);
+
+// Escribir el JSON actualizado de vuelta al archivo
+file_put_contents('../public/Monumentos.json', $jsonUpdated);
+
+
+
 header('Location: monumento.html?monumento=' . $idx);
 exit();
 
