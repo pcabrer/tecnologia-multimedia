c03db7f3b810ebee529e140565aae2c6d5d29dfc
pcabrer <p.cabrer.007@hotmail.es>
2023-05-31 20:53:25 +0200

    Pequeño Cambio en el php

--- contact.php
+++ contact.php
@@ -19,9 +19,8 @@
     $to = "p.cabrer.007@hotmail.es";
     $subject = "Checking PHP mail";
     $message = "PHP mail works just fine";
-    $headers = "From:" . $from;
-    //mail($to,$subject,$message, $headers);
-    echo "The email message was sent.";
+    mail($to,$subject,$message);
+    //echo "The email message was sent.";
 
 
     /*
