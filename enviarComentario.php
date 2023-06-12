35f074f19e51139a20912b89e348760fe8d6ecfc
Marc Melià <marcmelia10@gmail.com>
2023-06-12 18:57:50 +0200

    act82

--- enviarComentario.php
+++ enviarComentario.php
@@ -9,7 +9,7 @@ $name = $_POST['name'];
 $rating = $_POST['rating'];
 $comentario = $_POST['comment'];
 
-// Cargar el JSON existente
+// Cargar el JSON de valoraciones
 $json = file_get_contents('../public/assets/json/valoraciones.json');
 $data = json_decode($json, true);
 
@@ -29,22 +29,25 @@ $newItem = array(
     ),
 );
 
-// Agregar el nuevo elemento a la lista de valoraciones
+// Agregar la nueva valoración a la lista de valoraciones
 $data['itemListElement'][] = $newItem;
 
-// Convertir el arreglo de PHP en JSON y guardar el archivo
+// Convertir el arreglo de PHP en JSON y añadirlo al archivo valoraciones.json
 $newJson = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 file_put_contents('../public/assets/json/valoraciones.json', $newJson);
 
-// Cargar el JSON existente
+
+/*--------------- ACTUALIZAR VALORACIÓN MONUMENTO ------------------ */
+
+// Cargar el JSON de monumentos.
 $jsonMonumento = file_get_contents('../public/assets/json/Monumentos.json');
 $dataMonumento = json_decode($jsonMonumento, true);
 
 // Recorrer las reseñas y buscar el monumento con nombre "Catedral de Palma"
 foreach ($dataMonumento['itemListElement'] as &$item) {
     if ($item['name'] == $monumento) {
-        // Actualizar los valores de "ratingValue" y "reviewCount"
 
+        // Actualizar los valores de "ratingValue" y "reviewCount"
         $mediaValoraciones  = floatval($item['aggregateRating']['ratingValue']);
         $numeroValoraciones  = floatval($item['aggregateRating']['reviewCount']);
 
@@ -66,9 +69,8 @@ $jsonUpdated = json_encode($dataMonumento, JSON_PRETTY_PRINT| JSON_UNESCAPED_UNI
 // Escribir el JSON actualizado de vuelta al archivo
 file_put_contents('../public/assets/json/Monumentos.json', $jsonUpdated);
 
-
-
 header('Location: monumento.html?monumento=' . $idx);
+
 exit();
 
 ?>
